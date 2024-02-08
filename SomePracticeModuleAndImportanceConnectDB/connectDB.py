df5f95c90686a4569debdfee4e24b7f0fa2b7c9c
7460User <158508548+7460User@users.noreply.github.com>
2024-02-08 10:03:04 +0530

    Module

---
+++ SomePracticeModuleAndImportanceConnectDB/connectDB.py
@@ -0,0 +1,24 @@
+import cx_Oracle
+def  recordinsert():
+    while True:
+        try:
+            con=cx_Oracle.connect("system/tiger@localhost:1521/xe")
+            cur=con.cursor()
+            print("-"*30)
+            empno=int(input("Enter Employee Number:"))
+            ename=input("Enter Employee Name:")
+            sal=float(input("Enter Employee salry:"))
+            cname=input("Enter company name:")
+            print("-"*50) 
+            iq="insert into emp_table values(%d,'%s',%f,'%s')"
+            cur.execute(iq %(empno,ename,sal,cname))
+            con.commit()
+            print("cur data insrted")
+            print("-"*50)
+            ch=input("Do you want another insert(yes/no):")
+            if (ch.lower()=="no"):
+                break
+            
+        except cx_Oracle.DataBaseError:
+            print("file not found error:")
+recordinsert()            
