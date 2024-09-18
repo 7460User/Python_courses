ed2ec7f6a49c91813e7d5d44a414bb94832558ac
Govind Kumar <cs.govindkumar@gmail.com>
2024-09-18 22:13:46 +0530

    exp changes

--- EXCEPTION_HANDLING/Ex1.py
+++ EXCEPTION_HANDLING/Ex1.py
@@ -1,7 +1,4 @@
 l1=["govind","virat","rahul","rahul","virat","virat","govind","govind"]
-count_dict={}
-for i in l1:
-    if i in count_dict:
-        count_dict[i]+=1
-       else: hshdgdhsgd
+
+
         
