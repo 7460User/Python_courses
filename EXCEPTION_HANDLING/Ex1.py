928a164bd2d3dab202cff1c4394da10186c92f65
Govind Kumar <cs.govindkumar@gmail.com>
2024-09-18 22:25:32 +0530

    new change

--- EXCEPTION_HANDLING/Ex1.py
+++ EXCEPTION_HANDLING/Ex1.py
@@ -1,5 +1,11 @@
 l1=["govind","virat","rahul","rahul","virat","virat","govind","govind"]
 count_dict = {}
+for i in l1:
+    if i in count_dict:
+        count_dict[i]+=1
+    else:
+        count_dict[i]=1
+print(count_dict)         
 
 
         
